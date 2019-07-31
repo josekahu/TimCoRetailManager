17bd6b503734edf27e05d64f51f63e12b1934408
Pawel <pawel.czyz89@gmail.com>
2019-07-31 17:18:41 +0200

    Add Sales Page
    Adds the sales page XAML and the supporting
    View Model. Just scaffolds out the VM, does
    not add actual logic.

---
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,127 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDesktopUI.ViewModels
+{
+    public class SalesViewModel : Screen
+    {
+        private BindingList<string> _products;
+
+        public BindingList<string> Products
+        {
+            get { return _products; }
+            set
+            {
+                _products = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        private BindingList<string> vs;
+
+        public BindingList<string> Cart
+        {
+            get { return vs; }
+            set
+            {
+                vs = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
+
+        private string _itemQuantity;
+
+        public string ItemQuantity
+        {
+            get { return _itemQuantity; }
+            set
+            {
+                _itemQuantity = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+        
+        public string SubTotal
+        {
+            get
+            {
+                // TODO - Replace with calculation
+                return "$0.00";
+            }
+        }
+        public string Tax
+        {
+            get
+            {
+                // TODO - Replace with calculation
+                return "$0.00";
+            }
+        }
+
+        public string Total
+        {
+            get
+            {
+                // TODO - Replace with calculation
+                return "$0.00";
+            }
+        }
+
+        public bool CanAddToCart
+        {
+            get
+            {
+                bool output = false;
+
+                //Make sure something is selected
+                //Make sure there is an item quantity
+
+                return output;
+            }
+        }
+
+        public void AddToCart()
+        {
+
+        }
+
+        public bool CanRemoveFromCart
+        {
+            get
+            {
+                bool output = false;
+
+                //Make sure something is selected
+                //Make sure there is an item quantity
+
+                return output;
+            }
+        }
+        public void RemoveFromCart()
+        {
+
+        }
+
+        public bool CanCheckOut
+        {
+            get
+            {
+                bool output = false;
+
+                //Make sure something is in the cart
+
+                return output;
+            }
+        }
+        public void CheckOut()
+        {
+
+        }
+    }
+}
+
