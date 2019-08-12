a4f0ca2dd9df1c369420b8e8869970576649f6f3
Pawel <pawel.czyz89@gmail.com>
2019-08-12 23:02:18 +0200

    Update EF Database Name
    Updates EF Database to EFData

--- TRMdataManager/Models/IdentityModels.cs
+++ TRMdataManager/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace TRMdataManager.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("EFData", throwIfV1Schema: false)
         {
         }
         
